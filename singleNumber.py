858ca78e405b81ecc43b1a884d28970c9a9d5472
marcoArCha <marcoarcha@hotmail.com>
2023-12-06 18:15:10 -0600

    Solution

--- singleNumber.py
+++ singleNumber.py
@@ -3,3 +3,13 @@
 #You must implement a solution with a linear runtime 
 # complexity and use only constant extra space.
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        Set = set()
+        for element in nums:
+            if element not in Set:
+                Set.add(element)
+            else:
+                Set.remove(element)
+        single = Set.pop()
+        return single
